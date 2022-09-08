6d6db12ecb6dba1236576fda3718b4fc857053b8
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-09-08 16:47:27 -0300

    loren text added

--- src/components/LoremIpsumForm.jsx
+++ src/components/LoremIpsumForm.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react"
+import {data} from '../data/data'
 
 export function LoremIpsumForm(){
   const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
 
   function handleSubmit(e){
     e.preventDefault();
     console.log("hello world")
+    setText(data)
   }
 
   return(
@@ -19,6 +22,11 @@ export function LoremIpsumForm(){
         />
         <button className="btn-generator" type="submit">generate</button>
       </form>
+      <article>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
     </section>
   )
 }
